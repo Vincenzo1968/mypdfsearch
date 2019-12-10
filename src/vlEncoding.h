04294b66853317531debb47df732feba174322db
Vincenzo1968 <vincenzo.locicero@libero.it>
2019-12-10 21:01:27 +0100

    version 2.5.8.1

--- src/vlEncoding.h
+++ src/vlEncoding.h
@@ -1,3 +1,25 @@
+/*
+   Copyright (C) 2019 Vincenzo Lo Cicero
+
+   Author: Vincenzo Lo Cicero.
+   e-mail: vincenzo.locicero@libero.it
+          
+   This file is part of mypdfsearch.
+
+   mypdfsearch is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   mypdfsearch is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with mypdfsearch.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef	__MY_ENCODING_H
 #define	__MY_ENCODING_H
 
